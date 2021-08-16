abad867c67b7eb328f63b3f268118f9967d91ef8
nachshons <nachshons@wix.com>
2021-08-16 20:06:21 +0300

    touch

--- app.js
+++ app.js
@@ -326,8 +326,6 @@ function getTouches(evt) {
 }
 
 function handleTouchStart(evt) {
-    alert('touch')
-    document.getElementsByClassName('cell')[0].innerText=9;
     const firstTouch = getTouches(evt)[0];
     xDown = firstTouch.clientX;
     yDown = firstTouch.clientY;
