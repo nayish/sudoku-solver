f4080915bc31138b76eab1ef3cc7716d58a88da6
nachshons <nachshons@wix.com>
2021-08-16 20:03:00 +0300

    touch

--- app.js
+++ app.js
@@ -314,4 +314,68 @@ class SudokuSolver {
     }
 }
 
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            rightSwipe();
+        } else {
+            leftSwipe()
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            downSwipe()
+        } else {
+            upSwipe()
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
+
+function rightSwipe() {
+    document.getElementsByClassName('cell')[0]=9;
+}
+
+function leftSwipe() {
+
+}
+
+
+function downSwipe() {
+
+}
+
+function upSwipe() {
+
+}
+
+
 
